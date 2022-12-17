f3fab1c1fd0e3180c51d54698e41dc44da10d0cb
ESLAM <i.jamal.ict@btu.edu.eg>
2022-12-17 16:14:26 +0200

    Add a Picture and text in register screen

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -8,8 +8,57 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+            child: Column(
+              children:
+              [
+                Container(
+                  width: 200.0,
+                  height: 200.0,
+                  padding: const EdgeInsets.all(20.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.purple.shade50
+                  ),
+                  child: Image.asset(
+                    "assets/signUp.jpg"
+                  ),
+                ),
+                const SizedBox(height: 10,),
+                const Text(
+                  "Register",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "Add your phone number. We`ll send you a verification code",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black38,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20,),
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
