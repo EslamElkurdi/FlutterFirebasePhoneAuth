12438f6fe65448b12809e6971657536bd3c315b2
ESLAM <i.jamal.ict@btu.edu.eg>
2022-12-17 14:46:16 +0200

    Add Picture and Text in Welcome Screen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -11,6 +11,42 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+            child: Column(
+              children:
+              [
+                Image.asset(
+                  "assets/Sign.jpg"
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "Let`s get Started",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "Never a better time than now to start.",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black38,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
 
     );
   }
