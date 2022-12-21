121d63bcff0c37f2b6a3248d8f7eddda0bc5f67c
ESLAM <i.jamal.ict@btu.edu.eg>
2022-12-21 19:32:40 +0200

    Add PinPut in OTPScreen

--- lib/screens/otp_screen.dart
+++ lib/screens/otp_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
 
 class OTPScreen extends StatefulWidget {
   final String verificationId;
@@ -14,53 +15,64 @@ class _OTPScreenState extends State<OTPScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-            child: Center(
-                child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 25, horizontal: 35),
-                    child: SingleChildScrollView(
-                        child: Column(children: [
-                          Align(
-                            alignment: Alignment.topLeft,
-                            child: GestureDetector(
-                              onTap: () => Navigator.of(context).pop(),
-                              child: const Icon(Icons.arrow_back),
-                            ),
-                          ),
-                      Container(
-                        width: 200.0,
-                        height: 200.0,
-                        padding: const EdgeInsets.all(20.0),
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.purple.shade50),
-                        child: Image.asset("assets/signUp.jpg"),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      const Text(
-                        "Verification",
-                        style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
+            child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 25, horizontal: 35),
+                child: SingleChildScrollView(
+                    child: Column(children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: GestureDetector(
+                          onTap: () => Navigator.of(context).pop(),
+                          child: const Icon(Icons.arrow_back),
                         ),
                       ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      const Text(
-                        "Enter the OTP send to your phone number",
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.black38,
-                          fontWeight: FontWeight.bold,
+                  Container(
+                    width: 200.0,
+                    height: 200.0,
+                    padding: const EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.purple.shade50),
+                    child: Image.asset("assets/signUp.jpg"),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    "Verification",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Text(
+                    "Enter the OTP send to your phone number",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.black38,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                      Pinput(
+                        length: 6,
+                        showCursor: true,
+                        defaultPinTheme: PinTheme(
+                          width: 60,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(color: Colors.purple.shade200)
+                          )
                         ),
-                        textAlign: TextAlign.center,
                       ),
-                      const SizedBox(
-                        height: 20,
-                      )
-                    ]))))));
+                ])))));
   }
 }
