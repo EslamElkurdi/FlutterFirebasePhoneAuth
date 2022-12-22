6d4b5601c54060c81ca18129d8cdaf97a545a6f9
ESLAM <i.jamal.ict@btu.edu.eg>
2022-12-22 08:15:36 +0200

    Design AppBar in homeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
+import 'package:firebaseproject/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../provider/auth_provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,9 +14,20 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final ap = Provider.of<AuthProvider>(context, listen: false);
+
     return Scaffold(
-      body: Center(
-        child: Text("HOME SCREEN"),
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: const Text("FlutterPhone Auth"),
+        actions: [
+          IconButton(
+            onPressed: () {
+
+            },
+            icon: const Icon(Icons.exit_to_app),
+          ),
+        ],
       ),
     );
   }
